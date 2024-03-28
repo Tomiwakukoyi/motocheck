802f747af11915dcb62c83dbe40106fafdb36f55
tomiwakukoyi <tomiwakukoyiweb@gmail.com>
2024-03-28 21:24:47 +0100

    Navbar setup

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
+import HomePage from "@/pages/HomePage";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello</h1>
-    </main>
+    <div>
+      <HomePage />
+    </div>
   );
 }
---
+++ pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import Navbar from '@/component/Navbar'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <section>
+      <Navbar />
+    </section>
+  )
+}
+
+export default HomePage
