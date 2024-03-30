00b2c25103b4bab5ba1924a11f9105664684e3b0
tomiwakukoyi <tomiwakukoyiweb@gmail.com>
2024-03-30 15:42:17 +0100

    Navbar is just car logo and website name

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import CommuteIcon from '@mui/icons-material/Commute';
+
+const Navbar = () => {
+  return (
+    <nav className='flex items-center justify-center gap-2 merienda-font h-14 text-xl'>
+        <CommuteIcon />
+        <span>MotoCheck</span>
+    </nav>
+  )
+}
+
+export default Navbar
