03f46d0d6f6f86eaf8e4dfd4eef67568185aa800
tomiwakukoyi <tomiwakukoyiweb@gmail.com>
2024-04-03 22:43:29 +0100

    general info done

--- src/components/home/GeneralInfo.jsx
+++ src/components/home/GeneralInfo.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import GenInfoTemplate from "../GenInfoTemplate";
+import generalInfo from "@/public/generalInfo";
 
 const GeneralInfo = () => {
   return (
-    <div className="w-screen h-full bg-[#48595e] py-10 flex items-center justify-center">
-      <GenInfoTemplate />
+    <div className="w-screen h-full bg-[#48595e] py-10 flex flex-wrap gap-14 items-center justify-center">
+      {generalInfo.map(({ title, description }, index) => (
+        <GenInfoTemplate key={index} title={title} description={description} />
+      ))}
     </div>
   );
 };
