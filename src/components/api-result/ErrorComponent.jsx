67b1f6ce29d006044d6d769d4563b48ffeb97bd0
tomiwakukoyi <tomiwakukoyiweb@gmail.com>
2024-04-16 22:29:36 +0100

    Workig on the results page

--- src/components/api-result/ErrorComponent.jsx
+++ src/components/api-result/ErrorComponent.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import ErrorIcon from '@mui/icons-material/Error';
 
-const ErrorComponent = () => {
+const ErrorComponent = ({ error }) => {
   return (
-    <div className=" ">
-      <h1>Error</h1>
-      
+    <div className=" bg-gray-700 text-white p-3 py-4 w-300px flex flex-col items-center justify-center gap-2 rounded-md">
+      <ErrorIcon fontSize='' className=' text-6xl' />
+      <span className="text-center">Network error ! Try searching again...</span>
     </div>
   );
 };
--- src/components/home/SearchForm.jsx
+++ src/components/home/SearchForm.jsx
@@ -3,8 +3,9 @@ import React, { useState } from "react";
 import axios from "axios";
 import ResultPage from "@/app/data/page";
 import { useAppContext } from "@/context/searchContext";
-import Image from 'next/image'
+import Image from "next/image";
 import PlateNoDetails from "../api-result/PlateNoDetails";
+import ErrorComponent from "../api-result/ErrorComponent";
 
 const SearchForm = () => {
   const { setApiResult, setApiError } = useAppContext();
@@ -29,24 +30,42 @@ const SearchForm = () => {
           },
         }
       );
+      if (res) {
+        
+      }
       setApiResult(response.data);
-      console.log(response.data.information.registeredLGA, "this that");
+      console.log(response.data.information.registeredLGA, "this that tk");
     } catch (error) {
       console.log(error);
-      setApiError(
-        "An error occurred while fetching data. Please try again later."
-      );
+      // setApiError(
+      //   "An error occurred while fetching data. Please try again later."
+      // );
+      // Handle network errors specifically
+      if (error.code === "ERR_NETWORK") {
+        setApiError("Network Error: Please check your internet connection.");
+      } else if (apiResult === "undefined") {
+        setApiError("No results found for the entered plate number.");
+      } else {
+        setApiError(
+          "An error occurred while fetching data. Please try again later."
+        );
+      }
     }
   };
 
   return (
     <div className="w-screen flex flex-col items-center justify-center h-full py-10  bg-[#48595e] md:bg-carBg">
       <div className="h-300px flex flex-col gap-2 items-center justify-center w-full md:w-500px">
-        <span className="text-white mr-20">
+        <span className="text-white ">
           Enter first three digits of plate number:
         </span>
         <div className="flex items-center justify-center rounded-r-xl">
-          <Image src="/images/ng.png" width={100} height={100} className="h-14" />
+          <Image
+            src="/images/ng.png"
+            width={100}
+            height={100}
+            className="h-14"
+          />
           <input
             type="text"
             className="tracking-widest uppercase text-center focus:outline-none h-14 w-200px rounded-l-none rounded-xl pl-1 text-2xl font-bold"
@@ -56,13 +75,20 @@ const SearchForm = () => {
         </div>
 
         <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          className="bg-gray-500 hover:bg-gray-800 text-white font-semibold py-1 px-4 rounded"
           onClick={handleSubmit}
         >
           Search
         </button>
       </div>
-      {apiResult ? <ResultPage /> : <p>Loading......</p>}
+      {/* {apiResult ? (
+        <ResultPage />
+      ) : apiError ? (
+        <ErrorComponent error={apiError} />
+      ) : (
+        <p>Loading...</p>
+      )} */}
+      <ErrorComponent />
     </div>
   );
 };
